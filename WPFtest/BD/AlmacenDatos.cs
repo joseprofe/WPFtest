2448a07909de754ab4642731dbc842ab98b48223
Jose <Jose@DESKTOP-I47R5VA>
2020-11-30 18:25:50 +0100

    Primer commit con la app

---
+++ WPFtest/BD/AlmacenDatos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFtest
+{
+    class AlmacenDatos
+    {
+        public static List<Usuario> Usuarios { get; set; } = new List<Usuario>();
+    }
+}
---
+++ WPFtest/Pages/Login.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace WPFtest
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        Registro registration = new Registro();
+        Welcome welcome = new Welcome();
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+                ///Para conexiones de BD
+                /*
+                SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Registration where Email='" + email + "'  and password='" + password + "'", con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string username = dataSet.Tables[0].Rows[0]["FirstName"].ToString() + " " + dataSet.Tables[0].Rows[0]["LastName"].ToString();
+                    welcome.TextBlockName.Text = username;//Sending value from one form to another form.  
+                    welcome.Show();
+                    Close();
+                }
+                else
+                {
+                    errormessage.Text = "Sorry! Please enter existing emailid/password.";
+                }
+                con.Close();
+                */
+
+                if (AlmacenDatos.Usuarios.Any(u => u.Email == email && u.Password == password))
+                {
+                    var usuario = AlmacenDatos.Usuarios.FirstOrDefault(u => u.Email == email && u.Password == password);
+                    string username = usuario.Nombre + " " + usuario.Apellidos;
+                    welcome.TextBlockName.Text = username;//Sending value from one form to another form.  
+                    welcome.Show();
+                    Close();
+                }
+                else
+                {
+                    errormessage.Text = "Sorry! Please enter existing emailid/password.";
+                }
+            }
+        }
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            registration.Show();
+            Close();
+        }
+    }
+}
